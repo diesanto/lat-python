ce7ac1645b39658ef0905952da79806102a710c8
diesanto <di3.santo@gmail.com>
2021-02-25 22:34:16 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return 'Halaman Dashboard'
+
+@application.route('/post')
+def post():
+    return 'Ini Halaman Blog'
+
+if __name__ == '__main__':
+    application.run()
